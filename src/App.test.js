a22e107c677f5011feacdc66631904bf0be24e65
HubertTruong <hubert.truong01@gmail.com>
2024-02-05 17:23:22 +0100

    adding positinning of pieces

--- src/App.test.js
+++ src/App.test.js
@@ -7,6 +7,21 @@ test('render chessboard', ()=>{
     expect(screen.getByTestId('myChessboard')).toBeInTheDocument();
 });
 
+test('isPositionningValid', ()=>{
+  const startPosition = {
+    'a1': 'R', 'b1': 'N', 'c1': 'B', 'd1': 'Q', 'e1': 'K', 'f1': 'B', 'g1': 'N', 'h1': 'R',
+    'a2': 'P', 'b2': 'P', 'c2': 'P', 'd2': 'P', 'e2': 'P', 'f2': 'P', 'g2': 'P', 'h2': 'P',
+    'a7': 'p', 'b7': 'p', 'c7': 'p', 'd7': 'p', 'e7': 'p', 'f7': 'p', 'g7': 'p', 'h7': 'p',
+    'a8': 'r', 'b8': 'n', 'c8': 'b', 'd8': 'q', 'e8': 'k', 'f8': 'b', 'g8': 'n', 'h8': 'r',
+  };
+
+  render(<App />);
+
+  Object.entries(startPosition).forEach(([position, piece]) => {
+    const square = screen.getByTestId(position);
+    expect(square).toHaveTextContent(piece);
+  });
+});
 
 test('isValidMovePawn', ()=>{
   // init c2 c4 wP
